ca87e0f8be8414cffbc23fc42497c2637b67e0a0
YannickAiguier <yannick.aiguier@gmail.com>
2021-04-27 14:38:04 +0200

    Etape 5.3.1 : saisie du nombre de cartons à déménager au clavier

--- Demenagement/src/Demenagement/OnDemenage.java
+++ Demenagement/src/Demenagement/OnDemenage.java
@@ -1,25 +1,32 @@
 package Demenagement;
 
+import java.util.Scanner;
+
 public class OnDemenage {
 	public static void main(String[] args) {
-		int nbcartons = 34;
+		int nbcartons;
 		int nbrestants = 9;
 		int nbdanscamion = 0;
+		Scanner clavier = new Scanner(System.in);
+		
+		System.out.print("Nombre de cartons à déménager : ");
+		nbcartons = clavier.nextInt();
+		clavier.nextLine();
 		while (nbcartons > 0) {
-		  if (nbcartons < 9) {
-		    nbrestants = nbcartons;
-		  }
-		  while (nbdanscamion < nbrestants) {
-		    // mettre 1 carton dans le camion
-		    nbdanscamion = nbdanscamion + 1;
-		    nbcartons = nbcartons - 1;
-		  }
-		  if (nbdanscamion>1) {
-		    System.out.println("Un voyage de " + nbdanscamion + " cartons");
-		  } else {
-			  System.out.println("Un voyage de " + nbdanscamion + " carton");
-		  }
-		  nbdanscamion = 0;
+			if (nbcartons < 9) {
+				nbrestants = nbcartons;
+			}
+			while (nbdanscamion < nbrestants) {
+				// mettre 1 carton dans le camion
+				nbdanscamion = nbdanscamion + 1;
+				nbcartons = nbcartons - 1;
+			}
+			if (nbdanscamion > 1) {
+				System.out.println("Un voyage de " + nbdanscamion + " cartons");
+			} else {
+				System.out.println("Un voyage de " + nbdanscamion + " carton");
+			}
+			nbdanscamion = 0;
 		}
 	}
 }
